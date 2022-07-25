ae207f877c2d341a36fd0d7b10c508b1381769b9
MatValdejao <matvaldejao123@gmail.com>
2022-07-25 18:33:15 -0400

    generates HTML webpage

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length)
-
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem))
-}
+const fs = require("fs");
+const generatePage = require("./src/page-template")
 
 
-    
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
-printProfileData(profileDataArgs)
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+	if (err) throw err;
+
+	console.log("Portfolio complete! Check out index.html to see the output");
+});
